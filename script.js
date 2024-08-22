02e377acc71c8e7a6378de92b7ec608984884cc9
Teresa de Castro <teresa.decastro@hotmail.com>
2024-08-22 17:30:44 -0400

    able to randomize movement of food

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 // Define HTML elements
-const board = document.getElementById('game-board');
+const board = document.getElementById("game-board");
 
 // Define game variables;
-let snake = [{x: 10, y: 10}];
-// Draw game map, snake, food 
-function draw(){
-    board.innerHtml = '';
-    drawSnake();
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+
+// Draw game map, snake, food
+function draw() {
+  board.innerHtml = "";
+  drawSnake();
+  drawFood();
 }
 
 // Draw snake
 function drawSnake() {
-    snake.forEach((segment) => {
-        const snakeElement = createGameElement('div', 'snake');
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
     setPosition(snakeElement, segment);
     board.appendChild(snakeElement);
-});
+  });
 }
 
 // Create a snake or food cube/div
 function createGameElement(tag, className) {
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
 }
 
 // Set the position of the snake
-function setPosition(element, position){
-    element.style.gridColumn = position.x;
-    element.style.gridRow = position.y;
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
 }
 
-// Testing draw function 
+// Testing draw function
 draw();
+
+// Draw food function
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+// Generate food
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
