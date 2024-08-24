0cb16ce5662b6cea0ba352a82735f425175b114d
Teresa de Castro <teresa.decastro@hotmail.com>
2024-08-23 20:05:41 -0400

    Added updated score and collision

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const board = document.getElementById("game-board");
 const instructionText = document.getElementById("instruction-text");
 const logo = document.getElementById("logo");
+const score = document.getElementById("score");
 
 // Define game variables;
 const gridSize = 20;
@@ -17,6 +18,7 @@ function draw() {
   board.innerHTML = "";
   drawSnake();
   drawFood();
+  updateScore();
 }
 
 // Draw snake
@@ -85,7 +87,7 @@ function move() {
     clearInterval(gameInterval); // Clear past interval
     gameInterval = setInterval(() => {
       move();
-    //   checkCollision();
+    checkCollision();
       draw();
     }, gameSpeedDelay);
   } else {
@@ -106,7 +108,7 @@ function startGame() {
   logo.style.display = "none";
   gameIntervl = setInterval(() => {
     move();
-    // checkCollision();
+    checkCollision();
     draw();
   }, gameSpeedDelay);
 }
@@ -136,10 +138,42 @@ function handleKeyPress(event) {
   }
 }
 
-document.addEventListener('keydown', handleKeyPress);
+document.addEventListener("keydown", handleKeyPress);
 
-// function increaseSpeed() {
-//     if (gameSpeedDelay > 150) {
-//         gameSpeedDelay -= 5;
-//     }
-// }
+function increaseSpeed() {
+  if (gameSpeedDelay > 150) {
+    gameSpeedDelay -= 5;
+  } else if (gameSpeedDelay > 100) {
+    gameSpeedDelay -= 3;
+  } else if (gameSpeedDelay > 50) {
+    gameSpeedDelay -= 2;
+  } else if (gameSpeedDelay > 25) {
+    gameSpeedDelay -= 1;
+  }
+}
+
+function checkCollision() {
+  const head = snake[0];
+
+  if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
+    resetGame();
+  }
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y){
+        resetGame();
+    }
+  }
+}
+
+function resetGame() {
+    snake = [{ x: 10, y: 10 }];
+    food = generateFood();
+    direction = "right";
+    gameSpeedDelay = 200;
+    updateScore();
+}
+
+function updateScore() {
+    const currentScore = snake.length -1;
+    score.textContent = currentScore.toString().padStart(3,'0')
+}
